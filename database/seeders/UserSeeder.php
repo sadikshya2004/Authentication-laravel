5c8a96c6c77571dd231956d551e3897b25917617
2004sad <sadikshyaneupane999@gmail.com>
2026-06-14 13:32:40 +0545

    fix: Remove the role from the userseede.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,7 +17,6 @@ class UserSeeder extends Seeder
             'name' => 'Admin',
             'email' => 'admin@gmail.com',
             'password' => Hash::make('admin123'),
-            'role' => 'admin'
         ]);
     }
 }
