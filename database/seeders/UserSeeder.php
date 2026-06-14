91bdfdd883d756599e7e39c21f1fdb69f35cb98b
2004sad <sadikshyaneupane999@gmail.com>
2026-06-14 12:37:10 +0545

    fix:Add UserSeeder with default admin account

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -1,9 +1,9 @@
 <?php
 
 namespace Database\Seeders;
-
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class UserSeeder extends Seeder
 {
@@ -13,5 +13,11 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         //
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin123'),
+            'role' => 'admin'
+        ]);
     }
 }
