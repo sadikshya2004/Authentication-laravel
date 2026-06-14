d0309da41d1ba06d8db0b120187d4695668b9d7d
2004sad <sadikshyaneupane999@gmail.com>
2026-06-14 13:13:03 +0545

    Fix:Routes of login authentication

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 //here we define the routes for our application, linking them to the appropriate controller methods.
+
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/dashboard', [AuthController::class, 'dashboard'])->middleware('auth');
+Route::post('/logout', [AuthController::class, 'logout']);
+
 Route::get('/', function () {
     return redirect()->route('login');
 });
-Route::get('/', [AuthController::class, 'showLogin'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
-Route::get('/dashboard', [AuthController::class, 'dashboard'])->middleware('auth');
-Route::post('/logout', [AuthController::class, 'logout']);
