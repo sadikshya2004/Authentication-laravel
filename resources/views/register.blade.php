21db5d5a838f1b86d16f865d752051c78f36c317
2004sad <sadikshyaneupane999@gmail.com>
2026-06-17 20:30:41 +0545

    Create blade view for user registration.

---
+++ resources/views/register.blade.php
@@ -0,0 +1,19 @@
+<form method="POST" action="/register">
+    @csrf
+
+    <input type="text" name="name" placeholder="Name" required>
+
+    <input type="email" name="email" placeholder="Email" required>
+
+    <input type="password" name="password" placeholder="Password" required>
+
+    <input type="password" name="password_confirmation"
+           placeholder="Confirm Password" required>
+
+    <button type="submit">Register</button>
+</form>
+
+<p>
+    Already have an account?
+    <a href="/login">Login</a>
+</p>
