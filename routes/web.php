e98b822335cdf483904e92111486a9fdad8b8670
2004sad <sadikshyaneupane999@gmail.com>
2026-07-10 21:16:56 +0545

    Fix some routes of profile validation.

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProfileController;
 //here we define the routes for our application, linking them to the appropriate controller methods.
 
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
@@ -16,12 +17,20 @@ Route::post('/logout', [AuthController::class, 'logout']);
 
 Route::middleware(['auth'])->group(function () {
 Route::get('/dashboard', [AuthController::class, 'dashboard']);
-Route::get('/profile', [AuthController::class, 'profile'])
-->name('profile');
-Route::get('/profile/edit', [AuthController::class, 'editProfile'])->name('profile.edit'); 
-Route::put('/profile', [AuthController::class, 'updateProfile'])
-->name('profile.update');
+Route::get('/profile', [ProfileController::class, 'show'])
+    ->name('profile.show');
 
+Route::get('/profile/edit', [ProfileController::class, 'edit'])
+    ->name('profile.edit');
+
+Route::put('/profile/update', [ProfileController::class, 'update'])
+    ->name('profile.update');
+
+Route::get('/profile/password', [ProfileController::class, 'editPassword'])
+    ->name('profile.password');
+
+Route::put('/profile/password', [ProfileController::class, 'updatePassword'])
+    ->name('profile.password.update');
 Route::middleware(['role:admin'])->group(function () {
 Route::resource('users', UserController::class);
     });
