dfb030aa77eb42b96a56fc75a9185b94ec85690a
2004sad <sadikshyaneupane999@gmail.com>
2026-07-26 21:49:45 +0545

    Feat:Added products database and migration.

--- database/migrations/2026_07_26_153103_create_products_table.php
+++ database/migrations/2026_07_26_153103_create_products_table.php
@@ -13,7 +13,16 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('category_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->string('name');
+            $table->string('sku')->unique();
+            $table->text('description')->nullable();
+            $table->decimal('price', 10, 2);
+            $table->integer('stock_quantity')->default(0);
+            $table->string('image')->nullable();
+            $table->boolean('status')->default(true);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
